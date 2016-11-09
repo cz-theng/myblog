9dd2f6db12c9d1d43c725f493afe78e0fec1a073
cz-it <cz.theng@gmail.com>
2016-11-10 02:08:46 +0800

    uibuttion

--- blog/iOS/ui/uibutton/example/UIButtonDemo/UIButtonDemo/ViewController.swift
+++ blog/iOS/ui/uibutton/example/UIButtonDemo/UIButtonDemo/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let codeBtn:UIButton = UIButton(type:.system)
+        codeBtn.frame = CGRect(x:50, y:100, width:100, height:100)
+        codeBtn.setTitle("CodeButton", for: .normal)
+        self.view.addSubview(codeBtn)
+
     }
 
     override func didReceiveMemoryWarning() {
